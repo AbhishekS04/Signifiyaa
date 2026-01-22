730caf824af9d987211a7a49dcd742007c92915d
AbhishekS04 <abhishek23main@gmail.com>
2026-01-22 21:20:19 +0530

    .

--- babel.config.js
+++ babel.config.js
@@ -7,13 +7,13 @@ module.exports = function (api) {
         ],
         // Reanimated plugin disabled due to Metro bundler hanging at 99.9%
         // Animations will still work, just not optimized by babel transform
-        // plugins: [
-        //     [
-        //         "react-native-reanimated/plugin",
-        //         {
-        //             globals: ["__scanCodes"],
-        //         },
-        //     ],
-        // ],
+        plugins: [
+            [
+                "react-native-reanimated/plugin",
+                {
+                    globals: ["__scanCodes"],
+                },
+            ],
+        ],
     };
 };
